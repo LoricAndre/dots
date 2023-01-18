2717eeb0ce21429e6dbcb47f47ef45ffa79a02f3
Loric <loric@example.com>
2023-01-18 10:00:55 +0100

    fix snippet jump

--- nvim/lua/plugins/coq.lua
+++ nvim/lua/plugins/coq.lua
@@ -3,6 +3,7 @@ local imap = require'utils'.imap
 
 vim.g.coq_settings = {
   ['keymap.recommended'] = false,
+  ['keymap.jump_to_mark'] = '<C-j>',
   auto_start = 'shut-up'
 }
 
@@ -12,4 +13,3 @@ imap { '<BS>', 'pumvisible() ? "\\<C-e><BS>"  : "\\<BS>"', expr = true }
 imap { '<CR>', 'pumvisible() ? (complete_info().selected == -1 ? "\\<C-e><CR>" : "\\<C-y>") : "\\<CR>"', expr = true }
 imap { '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', expr = true }
 imap { '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<BS>"', expr = true }
-imap { '<C-k>', '<C-h>' }
